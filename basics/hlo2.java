eb95446069305733a3b30834cd918f5bfcfd7203
Komalika-jalluri <jallurikomalika2005@gmail.com>
2025-06-20 14:33:22 +0530

    Add files via upload

---
+++ basics/hlo2.java
@@ -0,0 +1,107 @@
+/*class Basics
+{
+public void name()
+{
+System.out.println("J.Komalika");
+}
+public static void main(String args[])
+{
+Basics b=new Basics();
+b.name();
+}
+}
+J.Komalika*/
+
+/*public class Basic
+{
+public static void main(String args[])
+{
+System.out.println("J.Komalika");
+}
+}
+J.Komalika*/
+
+/*class Hello
+{
+/**void name()
+{
+System.out.println("J.Komalika");
+}*/
+/*public static void main(String args[])
+{
+/*Hello h=new Hello();
+b.name();*/
+//System.out.println("pikachu");
+/*System.out.println("ECE");
+}
+}
+ECE*/
+
+/*class hi
+{
+public static void main(String args[])
+{
+int a=24;
+boolean b=true;
+char c='A';
+float d=3.14f;
+double e=99.99;
+System.out.println("integer value:"+a);
+System.out.println("boolean value:"+b);
+System.out.println("character value:"+c);
+System.out.println("float value:"+d);
+System.out.println("double value:"+e);
+}
+}
+integer value:24
+boolean value:true
+character value:A
+float value:3.14
+double value:99.99*/
+
+/*public class hlo1
+{
+int x=10;
+public void local(){
+int x=20;
+System.out.println("Local variable:"+x);
+}
+public static void main(String args[])
+{
+hlo1 h=new hlo1();
+h.local();
+System.out.print("Global variable: "+h.x);
+}
+}
+Local variable:20
+Global variable: 10*/
+
+class hlo2
+{
+public void name()
+{
+System.out.println("J.Komalika");
+}
+public static void main(String args[])
+{
+hlo2 h=new hlo2();
+h.name();
+}
+}
+J.Komalika
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
